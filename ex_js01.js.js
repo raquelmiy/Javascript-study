bd08b15efd1a47ec1ec899cd8dda9129f31ad253
Raquel Vitoria <rasousa555@gmail.com>
2022-02-03 15:17:13 -0300

    Add files via upload

---
+++ ex_js01.js.js
@@ -0,0 +1,13 @@
+//exercicio aritmético
+var n1 = prompt('Digite um número: ');
+var n2 = prompt('Digite um número: ');
+
+n1 = parseInt(n1);
+n2 = parseInt(n2); 
+
+document.write('A soma é : ' + (n1 + n2) + '</br>');
+document.write('A subtração é : ' + (n1 - n2) + '</br>');
+document.write('A multiplicação é : ' + (n1 * n2) + '</br>');
+document.write('A divisão é : ' + (n1 / n2) + '</br>');
+document.write('A média é : ' + (n1/n2)*2 + '</br>');
+
