bb1d50c52383a63b239322cd6e03d92efb652746
Raquel Vitoria <rasousa555@gmail.com>
2022-02-28 21:15:21 -0300

    Add files via upload

---
+++ exercicio.logica1.js
@@ -0,0 +1,11 @@
+//ESCREVA UMA FUNÇÃO QUE RECEBA 2 NÚMEROS E RETORNE O MAIOR DELES.
+
+		// function numeroMaior(x,y) {
+		// if (x > y) return x; return y;
+
+		//    return x > y ? x : y;
+		// }
+
+		const numeroMaior = (x, y) =>  x > y ? x : y;
+		
+		console.log(numeroMaior(5,1));
